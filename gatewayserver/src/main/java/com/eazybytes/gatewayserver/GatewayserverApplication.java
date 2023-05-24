162978d12fcf930abd33d866de2dde8e74f00112
Long Vũ <builongvu98@gmail.com>
2023-05-24 09:26:58 +0700

    up code

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -17,28 +17,45 @@ public class GatewayserverApplication {
         SpringApplication.run(GatewayserverApplication.class, args);
     }
 
-    @Autowired
-    private TokenRelayGatewayFilterFactory filterFactory;
+//    @Autowired
+//    private TokenRelayGatewayFilterFactory filterFactory;
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p.path("/eazybank/accounts/**")
+//                        .filters(f -> f.filters(filterFactory.apply())
+//                                .rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
+//                                .removeRequestHeader("Cookie"))
+//                        .uri("lb://ACCOUNTS"))
+//                .route(p -> p.path("/eazybank/loans/**")
+//                        .filters(f -> f.filters(filterFactory.apply())
+//                                .rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
+//                                .removeRequestHeader("Cookie"))
+//                        .uri("lb://LOANS"))
+//                .route(p -> p
+//                        .path("/eazybank/cards/**")
+//                        .filters(f -> f.filters(filterFactory.apply())
+//                                .rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
+//                                .removeRequestHeader("Cookie"))
+//                        .uri("lb://CARDS")).build();
+
         return builder.routes()
-                .route(p -> p.path("/eazybank/accounts/**")
-                        .filters(f -> f.filters(filterFactory.apply())
-                                .rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
-                                .removeRequestHeader("Cookie"))
-                        .uri("lb://ACCOUNTS"))
-                .route(p -> p.path("/eazybank/loans/**")
-                        .filters(f -> f.filters(filterFactory.apply())
-                                .rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
-                                .removeRequestHeader("Cookie"))
-                        .uri("lb://LOANS"))
                 .route(p -> p
-                        .path("/eazybank/cards/**")
-                        .filters(f -> f.filters(filterFactory.apply())
-                                .rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
-                                .removeRequestHeader("Cookie"))
-                        .uri("lb://CARDS")).build();
+                        .path("/eazybank/accounts/**")
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://ACCOUNTS")).
+                        route(p -> p
+                                .path("/eazybank/loans/**")
+                                .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
+                                        .addResponseHeader("X-Response-Time",new Date().toString()))
+                                .uri("lb://LOANS")).
+                        route(p -> p
+                                .path("/eazybank/cards/**")
+                                .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
+                                        .addResponseHeader("X-Response-Time",new Date().toString()))
+                                .uri("lb://CARDS")).build();
     }
 
 }
